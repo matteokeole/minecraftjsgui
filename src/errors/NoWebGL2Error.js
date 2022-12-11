09cf42c59505066ce5d9f90ef42d1e65ec346c85
matteokeole <matteoo34@icloud.com>
2022-12-11 19:47:34 +0100

    Added webgl.png and media prop on stylesheet

--- src/errors/NoWebGL2Error.js
+++ src/errors/NoWebGL2Error.js
@@ -1,5 +1,16 @@
 export function NoWebGL2Error() {
 	this.message = "It seems that your browser doesn't support WebGL2.";
+
+	const
+		div = document.createElement("div"),
+		img = document.createElement("img");
+
+	img.src = "assets/images/webgl.png";
+
+	div.classList.add("error");
+	div.append(img, this.message);
+
+	document.body.appendChild(div);
 }
 
 NoWebGL2Error.prototype = Error.prototype;
