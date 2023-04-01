d8bd2e7aaa5cdeb52ee2f4990f926107db8d7031
matteo@keole.net <matteoo34@icloud.com>
2023-04-01 17:16:17 +0200

    Convert WebGLRenderer and GUIRenderer to functions

--- public/layers/TestLayer.js
+++ public/layers/TestLayer.js
@@ -1,18 +1,11 @@
-import {Component, Layer} from "src/gui";
-import {Vector2} from "src/math";
+import {Layer} from "src/gui";
 import {extend} from "src/utils";
-import {Text} from "../components/index.js";
 
 export default function TestLayer() {
 	Layer.call(this);
 
 	/** @override */
-	this.build = () => [
-		new Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", {
-			align: Component.alignCenter,
-			margin: new Vector2(0, 0),
-		}),
-	];
+	this.build = () => [];
 }
 
 extend(TestLayer, Layer);
--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
-import Instance from "src/instance";
+import {Instance} from "src/instance";
 import {GUI, GUIRenderer} from "src/gui";
-import Layer from "./layers/MainMenuLayer.js";
-// import Layer from "./layers/TestLayer.js";
+import {MainMenuLayer as Layer} from "./layers/MainMenuLayer.js";
+// import {TestLayer as Layer} from "./layers/TestLayer.js";
 
 /**
  * @todo Fix undefined instance on throw
