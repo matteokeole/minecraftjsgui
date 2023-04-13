a2298269199d16ca7593bd9a327f3c40f91ab7c8
matteo@keole.net <matteoo34@icloud.com>
2023-04-13 19:42:05 +0200

    Remove 'colorMaskWeight'

--- assets/shaders/subcomponent.frag
+++ assets/shaders/subcomponent.frag
@@ -7,8 +7,7 @@ precision mediump sampler2DArray;
 
 flat in uint v_texture_index;
 in vec2 v_uv;
-in vec3 v_color_mask;
-in float v_color_mask_weight;
+in vec4 v_color_mask;
 
 uniform sampler2DArray u_sampler;
 
@@ -16,7 +15,7 @@ out vec4 FragColor;
 
 void main() {
 	vec4 texture = texture(u_sampler, vec3(v_uv, v_texture_index));
-	vec3 color = mix(texture.rgb, v_color_mask * NORMALIZE_HEX, v_color_mask_weight);
+	vec3 color = mix(texture.rgb, v_color_mask.rgb * NORMALIZE_HEX, v_color_mask.a);
 
 	FragColor = vec4(color, texture.a);
 }
--- public/components/Text.js
+++ public/components/Text.js
@@ -1,6 +1,6 @@
 import {Font} from "src";
 import {VisualComponent} from "src/gui";
-import {Vector2, Vector3} from "src/math";
+import {Vector2, Vector3, Vector4} from "src/math";
 import {extend} from "src/utils";
 import {guiComposite as context} from "../main.js";
 
@@ -32,10 +32,7 @@ export function Text(text, {font, color}) {
 		subcomponent = (fontCharacters[symbol] ?? fontCharacters[""]).clone();
 		subcomponent.setOffset(new Vector2(width, 0));
 
-		if (color) {
-			subcomponent.setColorMask(color);
-			subcomponent.setColorMaskWeight(1);
-		}
+		if (color) subcomponent.setColorMask(new Vector4(color[0], color[1], color[2], 1));
 
 		subcomponents.push(subcomponent);
 
